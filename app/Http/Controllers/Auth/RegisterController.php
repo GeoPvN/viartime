ca6befc66b1905505508c61a8c74264e2338de16
Iuri Sajaia <sajaiaiuri@gmail.com>
2018-06-25 17:21:23 +0400

    birthday - sex names

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,7 +63,6 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        // dd($data['fname']);
          $user = new User();
          $user->name = $data['name'];
          $user->fname = $data['fname'];
