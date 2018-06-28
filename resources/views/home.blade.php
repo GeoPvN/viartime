8d176e940086563578b8db6e2e9be9ab772e60ff
Iuri Sajaia <sajaiaiuri@gmail.com>
2018-06-28 16:34:45 +0400

    ჩათ-ბოტი

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+<nav class="navbar navbar-expand-md navbar-light navbar-laravel bg-light">
             
                 <a class="navbar-brand" href="{{ url('/') }}">
                 <a href="#"><img src="img/logo.png" alt="" class="home-logo"></a>
@@ -294,7 +294,7 @@
                     </div>  <!--home-center -->
                 </div>  <!-- center col -->
                     <div class="col-3"><div class="home-right">
-                        <div class="sidebar-contact-box">
+                        <!-- <div class="sidebar-contact-box">
                             <div class="contact-box-out">
                                 <h3>Contacts</h3>
                                 <div class="contact-box-in">
@@ -312,7 +312,62 @@
                                     </ul>
                                 </div>
                             </div>
-                        </div>
+                        </div> -->
+
+<section class="msger">
+  <header class="msger-header">
+    <div class="msger-header-title">
+      <i class="fas fa-comment-alt"></i> ViarTime-Chat
+    </div>
+    <div class="msger-header-options">
+      <span><i class="fas fa-cog"></i></span>
+    </div>
+  </header>
+
+  <main class="msger-chat">
+    <div class="msg left-msg">
+      <div
+       class="msg-img"
+       style="background-image: url(https://image.flaticon.com/icons/svg/327/327779.svg)"
+      ></div>
+
+      <div class="msg-bubble">
+        <div class="msg-info">
+          <div class="msg-info-name">BOT</div>
+          <div class="msg-info-time">12:45</div>
+        </div>
+
+        <div class="msg-text">
+          Hi, welcome to SimpleChat! Go ahead and send me a message. 😄
+        </div>
+      </div>
+    </div>
+
+    <div class="msg right-msg">
+      <div
+       class="msg-img"
+       style="background-image: url(https://image.flaticon.com/icons/svg/145/145867.svg)"
+      ></div>
+
+      <div class="msg-bubble">
+        <div class="msg-info">
+          <div class="msg-info-name">Iuri</div>
+          <div class="msg-info-time">12:46</div>
+        </div>
+
+        <div class="msg-text">
+          You can change your name in JS section!
+        </div>
+      </div>
+    </div>
+  </main>
+
+  <form class="msger-inputarea">
+    <input type="text" class="msger-input" placeholder="Enter your message...">
+    <button type="submit" class="msger-send-btn">Send</button>
+  </form>
+</section>
+
 
 
                 </div></div>
