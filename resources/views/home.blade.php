12345caec8a1bda4812ea66f262cdc61638923db
nikachikviladze <nikachikviladze93@gmail.com>
2018-06-29 10:53:52 +0400

    header layouts, chat users list

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,65 +15,7 @@
 <body>
 
 
-<nav class="navbar navbar-expand-md navbar-light navbar-laravel bg-light">
-            
-                <a class="navbar-brand" href="{{ url('/') }}">
-                <a href="#"><img src="img/logo.png" alt="" class="home-logo"></a>
-               
-               
-
-
-
-
-
-
-
-                
-
-                <input type="text" name="search" placeholder="Search..">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            
-        </nav>
+@include('layouts.header')
 
 <div id="app">
     <div class="post-toogle">
@@ -299,13 +241,9 @@
     <div class="content">
       <h2 class="title">Contacts</h2>
       <ul class="list" id="list">
-        <li class="list-item"><i class="fas fa-circle"> <span>Arron Afflalo   </span></i></li>
-        <li class="list-item"><i class="fas fa-circle"> <span>Jamel Artis     </span></i></li>
-        <li class="list-item"><i class="fas fa-circle"> <span>D.J. Augustin   </span></i></li>
-        <li class="list-item"><i class="fas fa-circle"> <span>Khem Birch      </span></i></li>
-        <li class="list-item"><i class="fas fa-circle"> <span>Bismack Biyombo </span></i></li>
-        <li class="list-item"><i class="fas fa-circle"> <span>Evan Fournier   </span></i></li>
-        <li class="list-item"><i class="fas fa-circle"> <span>Aaron Gordon    </span></i></li>
+        @foreach($users as $user)
+        <li class="list-item"><i class="fas fa-circle"> <span>{{$user->name}}   </span></i></li>
+        @endforeach
 
       </ul>
     </div>
